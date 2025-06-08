c567dc6f3340372494208fecc9f368835ad8285b
Alessandro Lemos Jr <ablemosjunior@gmail.com>
2025-06-08 18:06:33 -0300

    🚧 wip [invoice-issuing]: adds handling for charge update events with NF issuance logic - Implements a new event listener to handle charge updates, processing related entities and managing NF issuance logic. - Introduces validation for NFSe data completeness - Supports multiple NF statuses and integrates the necessary operations for NF issuance.

--- packages/twenty-server/src/modules/charges/charge.listener.ts
+++ packages/twenty-server/src/modules/charges/charge.listener.ts
@@ -10,6 +10,9 @@ import {
   FlattenedCompany,
   FlattenedPerson,
 } from 'src/modules/charges/types/inter';
+import { NfStatus } from 'src/modules/charges/types/NfStatus';
+import { NfType } from 'src/modules/charges/types/NfType';
+import { NFSe } from 'src/modules/charges/types/NotaFiscal.type';
 
 import { InterApiService } from './inter/inter-api.service';
 import { ChargeWorkspaceEntity } from './standard-objects/charge.workspace-entity';
@@ -174,4 +177,171 @@ export class ChargeEventListener {
       }),
     );
   }
+
+  @OnDatabaseBatchEvent('charge', DatabaseEventAction.UPDATED)
+  async handleChargeUpdateEvent(
+    payload: WorkspaceEventBatch<ObjectRecordCreateEvent>,
+  ) {
+    const { workspaceId, name: eventName, events } = payload;
+
+    if (!workspaceId || !eventName) {
+      this.logger.error(
+        `Missing workspaceId or eventName in payload ${JSON.stringify(payload)}`,
+      );
+
+      return;
+    }
+
+    const chargeRepository =
+      await this.twentyORMGlobalManager.getRepositoryForWorkspace<ChargeWorkspaceEntity>(
+        workspaceId,
+        'charge',
+      );
+
+    const charges = await Promise.all(
+      events.map((event) =>
+        chargeRepository.findOne({
+          where: { id: event.recordId },
+          relations: ['product', 'person', 'company'],
+        }),
+      ),
+    );
+
+    await Promise.all(
+      charges.map(async (charge) => {
+        if (!charge) {
+          this.logger.warn(`Charge not found for recordId: ${charge}`);
+
+          return;
+        }
+
+        const { company, product } = charge;
+
+        if (!product || !company) {
+          this.logger.warn(
+            `Charge ${charge.id} não possui relação com product ou company. Ignorando.`,
+          );
+
+          charge.nfStatus = NfStatus.DRAFT;
+
+          return;
+        }
+
+        try {
+          switch (charge.nfStatus) {
+            case NfStatus.DRAFT:
+              return;
+
+            case NfStatus.ISSUE:
+              this.issueNf(charge);
+              charge.nfStatus = NfStatus.ISSUED;
+
+              return;
+
+            case NfStatus.ISSUED:
+            case NfStatus.CANCELLED:
+              return this.issueNf(charge);
+
+            default:
+              this.logger.warn(
+                `Não foi possível em fazer a emissão da nota fiscal`,
+              );
+
+              return;
+          }
+        } catch (error) {
+          charge.nfStatus = NfStatus.DRAFT;
+          charge.requestCode = '';
+          this.logger.error(
+            `Erro processando charge ${charge.id}: ${error.message}`,
+            error.stack,
+          );
+        }
+
+        await chargeRepository.save(charge);
+      }),
+    );
+  }
+
+  private issueNf = (charge: ChargeWorkspaceEntity) => {
+    const { product, company } = charge;
+
+    if (!product || !company) return;
+
+    switch (charge.nfType) {
+      case NfType.NFSE: {
+        const nfse: NFSe = {
+          data_emissao: new Date().toISOString(),
+          prestador: {
+            cnpj: product.company?.cpfCnpj || '',
+            inscricao_municipal: product.company?.inscricaoMunicipal || '',
+            codigo_municipio: product.company?.codigoMunicipio || '',
+          },
+          tomador: {
+            cnpj: company.cpfCnpj || '',
+            razao_social: company.name,
+            email: company.emails.primaryEmail,
+            endereco: {
+              logradouro: company.address.addressStreet1,
+              numero: '-',
+              complemento: '-',
+              bairro: company.address.addressStreet2,
+              codigo_municipio: company.codigoMunicipio || '',
+              uf: company.address.addressCountry,
+              cep: company.address.addressZipCode,
+            },
+          },
+          servico: {
+            aliquota: charge.aliquotaIss,
+            discriminacao: charge.discriminacao,
+            iss_retido: charge.issRetido,
+            item_lista_servico: charge.itemListaServico,
+            codigo_tributario_municipio: charge.codigoTributarioMunicipio,
+            valor_servicos: (charge.price * (charge.percentNfse ?? 0)) / 100,
+          },
+        };
+
+        console.log('nfse: ', nfse);
+
+        if (!this.isNFSeComplete(nfse)) return;
+
+        // TODO: Create requisition to issue NFS-e
+
+        return;
+      }
+
+      case NfType.NFE:
+        return;
+    }
+  };
+
+  private isNFSeComplete = (nfse: NFSe): boolean => {
+    const dadosNotaCompletos = !!nfse.data_emissao;
+
+    const prestador =
+      !!nfse.prestador.cnpj &&
+      !!nfse.prestador.inscricao_municipal &&
+      !!nfse.prestador.codigo_municipio;
+
+    const tomador =
+      !!nfse.tomador.cnpj &&
+      !!nfse.tomador.razao_social &&
+      !!nfse.tomador.email &&
+      !!nfse.tomador.endereco.logradouro &&
+      !!nfse.tomador.endereco.numero &&
+      !!nfse.tomador.endereco.bairro &&
+      !!nfse.tomador.endereco.codigo_municipio &&
+      !!nfse.tomador.endereco.uf &&
+      !!nfse.tomador.endereco.cep;
+
+    const servico =
+      nfse.servico.aliquota !== undefined &&
+      !!nfse.servico.discriminacao &&
+      !!nfse.servico.iss_retido &&
+      !!nfse.servico.item_lista_servico &&
+      !!nfse.servico.codigo_tributario_municipio &&
+      nfse.servico.valor_servicos !== undefined;
+
+    return dadosNotaCompletos && prestador && tomador && servico;
+  };
 }
