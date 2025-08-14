98d93c05bf0a006ee87364c52f31e4bbd4c1b0e6
Eliezer Rodrigues <eliezer_gt.02@hotmail.com>
2025-08-14 17:24:22 -0300

    ✨ feat: Create worksapce tenant: - Addded workspaceId to udpate workspace repository operations to enshure that the listeners would have a way to indentify the workspace; - Added UserSubscriber to handle owner upsert;

---
+++ packages/twenty-server/src/engine/core-modules/kvoip-admin/standard-objects/tenant/subscribers/user.subscriber.ts
@@ -0,0 +1,59 @@
+import { Logger, OnModuleInit } from '@nestjs/common';
+import { ModuleRef } from '@nestjs/core';
+import { InjectDataSource } from '@nestjs/typeorm';
+
+import { isDefined } from 'twenty-shared/utils';
+import {
+  DataSource,
+  EntitySubscriberInterface,
+  EventSubscriber,
+  InsertEvent,
+  UpdateEvent,
+} from 'typeorm';
+
+import { OwnerService } from 'src/engine/core-modules/kvoip-admin/standard-objects/tenant/services/owner.service';
+import { User } from 'src/engine/core-modules/user/user.entity';
+
+@EventSubscriber()
+export class UserSubscriber
+  implements EntitySubscriberInterface<User>, OnModuleInit
+{
+  private readonly logger = new Logger(UserSubscriber.name);
+
+  private ownerService: OwnerService;
+
+  constructor(
+    private readonly moduleRef: ModuleRef,
+    @InjectDataSource('core')
+    private readonly dataSource: DataSource,
+  ) {
+    this.dataSource.subscribers.push(this);
+  }
+
+  async onModuleInit() {
+    this.ownerService = await this.moduleRef.resolve(OwnerService);
+  }
+
+  listenTo() {
+    return User;
+  }
+
+  async afterInsert(event: InsertEvent<User>) {
+    if (isDefined(event.entity.id) || isDefined(event.entity.email)) {
+      await this.ownerService.handleOwnerUpsert({
+        user: event.entity,
+      });
+    }
+  }
+
+  async afterUpdate(event: UpdateEvent<User>) {
+    if (
+      isDefined(event.entity) &&
+      (isDefined(event.entity.id) || isDefined(event.entity.creatorEmail))
+    ) {
+      await this.ownerService.handleOwnerUpsert({
+        user: event.entity as User,
+      });
+    }
+  }
+}
