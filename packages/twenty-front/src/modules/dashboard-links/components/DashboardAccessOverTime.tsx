b4f34dc35aa299008dc4e8c0b7b5ad16cc2cd2bd
Alessandro Lemos Jr <ablemosjunior@gmail.com>
2025-08-19 19:25:59 -0300

    ✨ feat: adds filter by time and location for new chart - Created data grouping code by platform type that accesses the trackable link to the new chart - Adds data filtering by time (week, month, and year) - Adds filtering by trackable link and location (country, state, and city) - Added generic functionality for filter return by location and default option - Adjusted `DashboardLinksPageContainer` for vertical overflow

---
+++ packages/twenty-front/src/modules/dashboard-links/components/DashboardAccessOverTime.tsx
@@ -0,0 +1,88 @@
+import { DashboardFilterDropdown } from '@/dashboard-links/components/ui/DashboardFilterDropdown';
+import { FilterType } from '@/dashboard-links/utils/filterLinkLogsData';
+import styled from '@emotion/styled';
+import {
+  Bar,
+  BarChart,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
+import { LinklogsChartData } from '~/types/LinkLogs';
+
+const StyledChartContainer = styled.div<{ hasData: boolean }>`
+  background: ${({ theme }) => theme.background.secondary};
+  height: ${({ hasData }) => (hasData ? '85%' : 'auto')};
+  padding: ${({ theme }) => theme.spacing(2)};
+  margin: ${({ theme }) => theme.spacing(4)};
+  border: 1px solid ${({ theme }) => theme.border.color.medium};
+  border-radius: 8px;
+`;
+
+const StyledHeader = styled.div`
+  align-items: center;
+  color: ${({ theme }) => theme.font.color.secondary};
+  display: flex;
+  justify-content: space-between;
+  padding: ${({ theme }) => theme.spacing(2)};
+`;
+
+const StyledText = styled.p`
+  color: ${({ theme }) => theme.font.color.secondary};
+  text-align: center;
+`;
+
+interface DashboardAccessOverTimeProps {
+  chartData: LinklogsChartData;
+  onFilterChange: (filter: FilterType) => void;
+}
+
+export const DashboardAccessOverTime = ({
+  chartData: { data, sourceKeyColors },
+  onFilterChange,
+}: DashboardAccessOverTimeProps) => {
+  const hasData = data.length > 0;
+
+  return (
+    <StyledChartContainer hasData={hasData}>
+      <StyledHeader>
+        <label>Acessos ao longo do tempo</label>
+        <DashboardFilterDropdown
+          onChange={onFilterChange}
+          scopeKey="dashboard-filter-access-over-time"
+        />
+      </StyledHeader>
+      {hasData ? (
+        <ResponsiveContainer>
+          <BarChart
+            data={data}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 60,
+            }}
+          >
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend align="left" />
+            {Object.entries(sourceKeyColors).map(([key, color]) => (
+              <Bar
+                key={key}
+                dataKey={key}
+                stackId="a"
+                fill={color}
+                name={key}
+              />
+            ))}
+          </BarChart>
+        </ResponsiveContainer>
+      ) : (
+        <StyledText>Nenhum dado para exibir</StyledText>
+      )}
+    </StyledChartContainer>
+  );
+};
--- packages/twenty-front/src/pages/dashboard-links/DashboarLinks.tsx
+++ packages/twenty-front/src/pages/dashboard-links/DashboarLinks.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-imports */
 /* eslint-disable @nx/workspace-explicit-boolean-predicates-in-if */
-import { IconChartBar, IconSearch } from '@tabler/icons-react';
+import { IconChartBar } from '@tabler/icons-react';
 
 import { DashboardLinksChart } from '@/dashboard-links/components/DashboardLinksChart';
 import { DashboardPageContainer } from '@/dashboard-links/components/ui/DashboardLinksPageContainer';
@@ -13,28 +13,37 @@ import {
   filterLinkLogsData,
   FilterType,
 } from '@/dashboard-links/utils/filterLinkLogsData';
-import { TextInput } from '@/ui/input/components/TextInput';
 import { PageContainer } from '@/ui/layout/page/components/PageContainer';
 import { useQuery } from '@apollo/client';
 import styled from '@emotion/styled';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
+
+import { DashboardAccessOverTime } from '@/dashboard-links/components/DashboardAccessOverTime';
+import { generateSelectOptions } from '@/dashboard-links/utils/generateSelectOptions';
+import { groupPlatformsOverTime } from '@/dashboard-links/utils/groupLinkLogsAccessOverTimeData';
+import { Select } from '@/ui/input/components/Select';
 import { isDefined } from 'twenty-shared/utils';
-import { H2Title } from 'twenty-ui/display';
-import { Section } from 'twenty-ui/layout';
 import { Query } from '~/generated-metadata/graphql';
 import { groupLinkLogsData } from '~/utils/groupLinkLogsData';
 
-const StyledSection = styled(Section)`
-  padding: ${({ theme }) => theme.spacing(4)};
-`;
-
-const StyledSearchInput = styled(TextInput)`
-  padding-bottom: ${({ theme }) => theme.spacing(2)};
+const StyledSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: ${({ theme }) => theme.spacing(2)};
+  padding-inline: ${({ theme }) => theme.spacing(4)};
+  padding-top: ${({ theme }) => theme.spacing(4)};
 `;
 
 export const DashboardLinks = () => {
-  const [searchTerm, setSearchTerm] = useState('');
   const [filter, setFilter] = useState<FilterType>('week');
+  const [filterAccessOverTime, setFilterAccessOverTime] =
+    useState<FilterType>('week');
+
+  const [selectedLink, setSelectedLink] = useState<string>('none');
+  const [selectedCountry, setSelectedCountry] = useState<string>('none');
+  const [selectedState, setSelectedState] = useState<string>('none');
+  const [selectedCity, setSelectedCity] = useState<string>('none');
 
   const { data, loading, error } = useQuery<
     Pick<Query, 'getDashboardLinklogs'>
@@ -45,9 +54,60 @@ export const DashboardLinks = () => {
       cause: error?.cause ?? JSON.stringify(data),
     });
 
-  const rawData = data?.getDashboardLinklogs ?? [];
+  const rawData = useMemo(() => data?.getDashboardLinklogs ?? [], [data]);
   const filteredData = filterLinkLogsData(rawData, filter);
   const chartData = groupLinkLogsData(filteredData);
+
+  const filterLinkOptions = generateSelectOptions(
+    rawData,
+    'linkName',
+    'Selecione um link',
+  );
+
+  const filterCountryOptions = generateSelectOptions(
+    rawData,
+    'country',
+    'Selecione um país',
+  );
+
+  const filterStateOptions = generateSelectOptions(
+    rawData,
+    'regionName',
+    'Selecione um estado',
+  );
+
+  const filterCityOptions = generateSelectOptions(
+    rawData,
+    'city',
+    'Selecione uma cidade',
+  );
+
+  const filteredBySelection = useMemo(() => {
+    let result = rawData;
+
+    if (selectedLink !== 'none') {
+      result = result.filter((item) => item.linkName === selectedLink);
+
+      if (selectedCountry !== 'none') {
+        result = result.filter((item) => item.country === selectedCountry);
+      }
+      if (selectedState !== 'none') {
+        result = result.filter((item) => item.regionName === selectedState);
+      }
+      if (selectedCity !== 'none') {
+        result = result.filter((item) => item.city === selectedCity);
+      }
+    } else {
+      result = [];
+    }
+
+    return result;
+  }, [rawData, selectedLink, selectedCountry, selectedState, selectedCity]);
+
+  const chartDataAccessOverTime = groupPlatformsOverTime(
+    filteredBySelection,
+    filterAccessOverTime,
+  );
 
   return (
     <PageContainer>
@@ -62,17 +122,55 @@ export const DashboardLinks = () => {
                 chartData={chartData}
                 onFilterChange={setFilter}
               />
-              <StyledSection fullWidth>
-                <H2Title title="All links" />
-
-                <StyledSearchInput
-                  LeftIcon={IconSearch}
-                  placeholder="Search for an object..."
-                  value={searchTerm}
-                  onChange={setSearchTerm}
+              <StyledSection>
+                <Select
+                  dropdownId="filter-link"
+                  options={filterLinkOptions}
+                  value={selectedLink}
+                  onChange={(val) => {
+                    setSelectedLink(String(val));
+                    setSelectedCountry('none');
+                    setSelectedState('none');
+                    setSelectedCity('none');
+                  }}
                   fullWidth
                 />
+                <Select
+                  dropdownId="filter-country"
+                  options={filterCountryOptions}
+                  value={selectedCountry}
+                  onChange={(val) => {
+                    setSelectedCountry(String(val));
+                    setSelectedState('none');
+                    setSelectedCity('none');
+                  }}
+                  fullWidth
+                  disabled={selectedLink === 'none'}
+                />
+                <Select
+                  dropdownId="filter-state"
+                  options={filterStateOptions}
+                  value={selectedState}
+                  onChange={(val) => {
+                    setSelectedState(String(val));
+                    setSelectedCity('none');
+                  }}
+                  fullWidth
+                  disabled={selectedLink === 'none'}
+                />
+                <Select
+                  dropdownId="filter-city"
+                  options={filterCityOptions}
+                  value={selectedCity}
+                  onChange={(val) => setSelectedCity(String(val))}
+                  fullWidth
+                  disabled={selectedLink === 'none'}
+                />
               </StyledSection>
+              <DashboardAccessOverTime
+                chartData={chartDataAccessOverTime}
+                onFilterChange={setFilterAccessOverTime}
+              />
             </>
           )}
         </DashboardPageContainer>
