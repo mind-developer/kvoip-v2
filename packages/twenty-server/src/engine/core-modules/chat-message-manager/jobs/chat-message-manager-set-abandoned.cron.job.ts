984fcb64f0456df0ed77f4bf89667962229dc3d4
Caleb Durães <calebduraes@icloud.com>
2025-10-22 17:08:07 -0300

    chore: minor refactors

--- packages/twenty-server/src/engine/core-modules/chat-message-manager/jobs/chat-message-manager-set-abandoned.cron.job.ts
+++ packages/twenty-server/src/engine/core-modules/chat-message-manager/jobs/chat-message-manager-set-abandoned.cron.job.ts
@@ -1,16 +1,12 @@
-import { Logger, Scope } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 import { InjectMessageQueue } from 'src/engine/core-modules/message-queue/decorators/message-queue.decorator';
 import { Process } from 'src/engine/core-modules/message-queue/decorators/process.decorator';
 import { Processor } from 'src/engine/core-modules/message-queue/decorators/processor.decorator';
 import { MessageQueue } from 'src/engine/core-modules/message-queue/message-queue.constants';
 import { MessageQueueService } from 'src/engine/core-modules/message-queue/services/message-queue.service';
-import { TwentyORMGlobalManager } from 'src/engine/twenty-orm/twenty-orm-global.manager';
 import { ChatAbandonmentService } from '../services/chat-abandonment.service';
 
-@Processor({
-  queueName: MessageQueue.cronQueue,
-  scope: Scope.DEFAULT,
-})
+@Processor(MessageQueue.cronQueue)
 export class ChatMessageManagerSetAbandonedCronJob {
   private hasRunOnce: boolean = false;
   private readonly logger = new Logger(
@@ -18,7 +14,6 @@ export class ChatMessageManagerSetAbandonedCronJob {
   );
 
   constructor(
-    private readonly twentyORMGlobalManager: TwentyORMGlobalManager,
     private readonly chatAbandonmentService: ChatAbandonmentService,
     @InjectMessageQueue(MessageQueue.cronQueue)
     private readonly messageQueueService: MessageQueueService,
@@ -38,15 +33,15 @@ export class ChatMessageManagerSetAbandonedCronJob {
       return;
     }
 
+    await this.chatAbandonmentService.handleChatAbandonment(
+      chatId,
+      workspaceId,
+    );
     // Cancel the scheduled abandonment job
     await this.messageQueueService.removeCron({
       jobName: 'ChatMessageManagerSetAbandonedCronJob',
       jobId: chatId,
     });
-    await this.chatAbandonmentService.handleChatAbandonment(
-      chatId,
-      workspaceId,
-    );
 
     this.logger.log(
       `Chat ${chatId} marked as abandoned and cron job cancelled`,
