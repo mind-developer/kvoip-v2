33c3de2e312c68fba604ca8e9808318f0051cd88
Caleb Durães <calebduraes@icloud.com>
2025-10-31 17:06:14 -0300

    feat(meta): improve chatbot action menu state management

--- packages/twenty-front/src/modules/chatbot/components/actions/ChatbotActionMenu.tsx
+++ packages/twenty-front/src/modules/chatbot/components/actions/ChatbotActionMenu.tsx
@@ -1,6 +1,9 @@
+import { useHandleNodeValue } from '@/chatbot/hooks/useHandleNodeValue';
+import { isChatbotActionMenuOpenState } from '@/chatbot/state/isChatbotActionMenuOpen';
 import { SelectableList } from '@/ui/layout/selectable-list/components/SelectableList';
 import { SelectableListItem } from '@/ui/layout/selectable-list/components/SelectableListItem';
 import styled from '@emotion/styled';
+import { useSetRecoilState } from 'recoil';
 import {
   IconFileImport,
   IconHierarchy,
@@ -14,16 +17,26 @@ const StyledSelectableList = styled(SelectableList)`
   z-index: 10;
 `;
 const StyledChatbotActionMenuContainer = styled.div`
-  background-color: ${({ theme }) => theme.background.quaternary};
+  background-color: ${({ theme }) => theme.background.tertiary};
   padding: ${({ theme }) => theme.spacing(3)};
   border-radius: ${({ theme }) => theme.border.radius.sm};
   border: 1px solid ${({ theme }) => theme.border.color.medium};
+  box-shadow: ${({ theme }) => theme.boxShadow.light};
   position: absolute;
   top: 0;
   left: 0;
 `;
 
 export const ChatbotActionMenu = () => {
+  const { addNode } = useHandleNodeValue();
+  const setIsChatbotActionMenuOpen = useSetRecoilState(
+    isChatbotActionMenuOpenState,
+  );
+
+  const closeChatbotActionMenu = () => {
+    setIsChatbotActionMenuOpen(false);
+  };
+
   const selectableItemIdArray = [
     'add-text-node',
     'add-image-node',
@@ -40,16 +53,44 @@ export const ChatbotActionMenu = () => {
       >
         <Label>Add a node</Label>
         <SelectableListItem itemId="add-text-node">
-          <MenuItem LeftIcon={IconTextSize} text="+ Text node" />
+          <MenuItem
+            onClick={() => {
+              addNode('text');
+              closeChatbotActionMenu();
+            }}
+            LeftIcon={IconTextSize}
+            text="+ Text node"
+          />
         </SelectableListItem>
         <SelectableListItem itemId="add-text-node">
-          <MenuItem LeftIcon={IconPhoto} text="+ Image node" />
+          <MenuItem
+            onClick={() => {
+              addNode('image');
+              closeChatbotActionMenu();
+            }}
+            LeftIcon={IconPhoto}
+            text="+ Image node"
+          />
         </SelectableListItem>
         <SelectableListItem itemId="add-text-node">
-          <MenuItem LeftIcon={IconFileImport} text="+ File node" />
+          <MenuItem
+            onClick={() => {
+              addNode('file');
+              closeChatbotActionMenu();
+            }}
+            LeftIcon={IconFileImport}
+            text="+ File node"
+          />
         </SelectableListItem>
         <SelectableListItem itemId="add-text-node">
-          <MenuItem LeftIcon={IconHierarchy} text="+ Conditional node" />
+          <MenuItem
+            onClick={() => {
+              addNode('conditional');
+              closeChatbotActionMenu();
+            }}
+            LeftIcon={IconHierarchy}
+            text="+ Conditional node"
+          />
         </SelectableListItem>
       </StyledSelectableList>
     </StyledChatbotActionMenuContainer>
