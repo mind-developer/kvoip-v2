dc89920e872012b7195b1140f0a66f48406d391a
Caleb Durães <caleb@crm.gratis>
2025-11-05 11:39:19 -0300

    fix(meta): adjust headers remove content-type

--- packages/twenty-front/src/modules/chat/client-chat/hooks/useClientChatSubscription.ts
+++ packages/twenty-front/src/modules/chat/client-chat/hooks/useClientChatSubscription.ts
@@ -39,7 +39,7 @@ export const useClientChatSubscription = ({
       headers: {
         Authorization: token ? `Bearer ${token}` : '',
         'X-Accel-Buffering': 'no',
-        'Content-Type': 'text/event-stream',
+        // 'Content-Type': 'text/event-stream',
       },
     });
   }, [tokenPair?.accessOrWorkspaceAgnosticToken?.token]);
