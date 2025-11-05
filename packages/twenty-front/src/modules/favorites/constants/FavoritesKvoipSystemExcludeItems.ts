5d24c66677b846b56f30760b108ad645cc4e7775
DiegoKvoip <diego@crm.gratis>
2025-11-05 17:14:26 -0300

    refactor(favorites): Simplify FAVORITES_KVOIP_SYSTEM_EXCLUDE_ITEMS by removing accounts payable and receivable entries

--- packages/twenty-front/src/modules/favorites/constants/FavoritesKvoipSystemExcludeItems.ts
+++ packages/twenty-front/src/modules/favorites/constants/FavoritesKvoipSystemExcludeItems.ts
@@ -1,8 +1,1 @@
-export const FAVORITES_KVOIP_SYSTEM_EXCLUDE_ITEMS = [
-  'traceables',
-  'tenants',
-  /* @kvoip-woulz proprietary:begin */
-  'accountsPayable',
-  'accountsReceivable',
-  /* @kvoip-woulz proprietary:end */
-];
+export const FAVORITES_KVOIP_SYSTEM_EXCLUDE_ITEMS = ['traceables', 'tenants'];
